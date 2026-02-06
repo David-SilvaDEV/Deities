8aebc7fee2fd40c4565ba185445bfb76c6e2967a
David-Silva <orpi4488@gmail.com>
2026-02-06 16:21:06 -0500

    arreglo de imagen del footer

--- src/app/Components/layout/layout.ts
+++ src/app/Components/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Body } from '../body/body';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, Body ],
+  imports: [Header, Body, Footer ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
